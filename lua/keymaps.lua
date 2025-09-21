fe7aff3c91c6ac23aff550a3b55894b1146c6c86
Nhan <nhanphamthe2410@gmail.com>
2025-09-21 19:33:21 +0700

    leader b to build and run inside nvim terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,8 +1,16 @@
 -- Keymaps
 local map = vim.keymap.set
-
-map("n", "<Space>f", ":find ")         -- find file
-map("n", "<Space>n", ":nohlsearch<CR>")-- clear highlight
-map("n", "<Space>h", ":split<CR>")
-map("n", "<Space>v", ":vsplit<CR>")
+map("n", "<C-h>","<C-w>h" )
+map("n", "<C-l>","<C-w>l" )
+map("n", "<leader>f", ":find ")         -- find file
+map("n", "<leader>n", ":nohlsearch<CR>")-- clear highlight
+map("n", "<leader>h", ":split<CR>")
+map("n", "<leader>v", ":vsplit<CR>")
 vim.g.mapleader = ' ' 
+-- Compile & run C++ file
+vim.keymap.set("n", "<leader>b", function()
+  local file = vim.fn.expand("%:p")       -- file hiện tại (full path)
+  local output = vim.fn.expand("%:r")     -- tên file bỏ đuôi (.cpp -> main)
+  vim.cmd("botright 15split | terminal g++ \"" .. file .. "\" -o \"" .. output .. "\" && \"" .. output .. "\"")
+end, { desc = "Build & run C++ file" })
+
